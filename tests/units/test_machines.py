c486f2433029740639c96438a69368eaec68d8e4
marcArisen <markjingnum9@gmail.com>
2023-02-01 13:08:15 +0700

    fix unit test code

--- tests/units/test_machines.py
+++ tests/units/test_machines.py
@@ -24,39 +24,42 @@ class TestMachines(unittest.TestCase):
             assert len(response_json["stock"]) == 0
             return response_json["id"]
 
-    # def test_get_machine_by_id(self):
-    #     random_machine_id = randrange(10)
-    #     response = requests.get(
-    #         url=f"{self.machine_api}/{random_machine_id}",
-    #     )
-    #     response_json = response.json()
-    #     assert response_json["id"] == random_machine_id
-    #     assert response_json["name"] is not None
-    #     assert response_json["location"] is not None
-    #     assert response_json["stock"] is not None
-    #
-    # def test_get_all_machines(self):
-    #     response = requests.get(
-    #         url=f"{self.machine_api}",
-    #     )
-    #     response_json = response.json()
-    #     for machine in response_json:
-    #         assert machine["id"] is not None
-    #         assert machine["name"] is not None
-    #         assert machine["location"] is not None
-    #         assert machine["stock"] is not None
-    #
-    # def test_update_stock(self):
-    #     random_machine_id = randrange(10)
-    #     item_name = lorem.sentence()
-    #     random_item_amount = randrange(10)
-    #     response = requests.put(
-    #         url=f"{self.machine_api}/{random_machine_id}",
-    #         json={item_name: random_item_amount},
-    #     )
-    #     response_json = response.json()
-    #     assert item_name in response_json["stock"]
-    #     assert response_json["stock"][item_name] >= random_item_amount
+    def test_get_machine_by_id(self):
+        # random_machine_id = randrange(10)
+        random_machine_id = 3
+        flask_app = create_app()
+        with flask_app.test_client() as test_client:
+            response = test_client.get(f"/machines/{random_machine_id}")
+            response_json = response.get_json()
+            assert response_json["id"] == random_machine_id
+            assert response_json["name"] is not None
+            assert response_json["location"] is not None
+            assert response_json["stock"] is not None
+
+    def test_get_all_machines(self):
+        flask_app = create_app()
+        with flask_app.test_client() as test_client:
+            response = test_client.get("/machines")
+            response_json = response.get_json()
+            for machine in response_json:
+                assert machine["id"] is not None
+                assert machine["name"] is not None
+                assert machine["location"] is not None
+                assert machine["stock"] is not None
+
+    def test_update_stock(self):
+        # random_machine_id = randrange(10)
+        random_machine_id = 2
+        item_name = lorem.sentence()
+        random_item_amount = randrange(10)
+        flask_app = create_app()
+        with flask_app.test_client() as test_client:
+            response = test_client.put(f"/machines/{random_machine_id}", json={item_name: random_item_amount})
+            response_json = response.get_json()
+            print(response_json)
+            assert response.status_code == 200
+            assert item_name in response_json["stock"]
+            assert response_json["stock"][item_name] >= random_item_amount
 
 
 if __name__ == "__main__":
